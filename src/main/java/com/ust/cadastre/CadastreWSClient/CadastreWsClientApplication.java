72427d6ef684c800232aa337ed378839def420fb
raulhsj <Raul.Hernandez@ust-global.com>
2018-07-30 13:03:51 +0200

    Added resources: \CadastreWSClient\mvnw \CadastreWSClient\mvnw.cmd \CadastreWSClient\pom.xml \CadastreWSClient\src\main\java\com\ust\cadastre\CadastreWSClient\CadastreWsClientApplication.java \CadastreWSClient\src\main\resources\application.properties \CadastreWSClient\src\test\java\com\ust\cadastre\CadastreWSClient\CadastreWsClientApplicationTests.java

---
+++ src/main/java/com/ust/cadastre/CadastreWSClient/CadastreWsClientApplication.java
@@ -0,0 +1,12 @@
+package com.ust.cadastre.CadastreWSClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastreWsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastreWsClientApplication.class, args);
+	}
+}
